92d8080e6f96f55be0d68923a121d5af9c35223b
OnePailas <tuwgvfc@gmail.com>
2024-11-29 22:57:04 +0500

    Add files via upload

--- Calculator/DefCalc.py
+++ Calculator/DefCalc.py
@@ -1,1 +1,21 @@
-
+def calc():
+    chislo1 = float(input('Введите первое число: '))
+    chislo2 = float(input('Введите второе число: '))
+    print()
+    result = chislo1 + chislo2
+    print(f'Результат сложения: {result}')
+    result = chislo1 - chislo2 
+    print(f'Результат вычитания: {result}')
+    result = chislo1 * chislo2
+    print(f'Результат умножения: {result}')
+    if chislo2 != 0:
+        result = chislo1 % chislo2
+        print(f'Результат деления: {result}')
+    else:
+        print()
+        print('На ноль делить нельзя')
+        chislo2 = float(input('Введите второе число: '))
+        result = chislo1 % chislo2
+        print()
+        print(f'Результат деления: {result}')
+calc()
